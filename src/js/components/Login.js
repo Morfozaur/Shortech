567fd7f565f40a2ed04425a7c7f6a0618623402e
Morfozaur <e.borzechowski@gmail.com>
2021-08-12 13:46:19 +0200

    Rozbicie sekcji logowania

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import LoginForm from "./elements/LoginForm";
 import {useDispatch, useSelector} from "react-redux";
-import firebase from "firebase/app";
-import {useHistory} from "react-router-dom";
-import {fetchDemo} from "../redux/actions/allFetchers";
+
+import LoginInfo from "./elements/LoginInfo";
 
 const Login = () => {
-    const history = useHistory();
-    const dispatch = useDispatch();
+
     const isLogged = useSelector(state => state.log);
     const isDemo = useSelector(state => state.demo);
 
-    const logout = () => {
-        firebase.auth().signOut().catch(err => console.error(err.message));
-        dispatch(fetchDemo(false));
-    };
-
     return (
         <div className="login">
-            {(isLogged || isDemo) &&
-            <div className='login-status'>
-                <h2>Zalogowano jako:</h2>
-                <h3>{isDemo ? 'Wersja demo' : 'Administrator'}</h3>
-                {isDemo &&
-                <div className='login-info' >
-                    <p>W wersji demonstracyjnej dostęp do edytora i kreatora postów jest ograniczony. Wpisy modyfikowane są wyłącznie w ramach drzewa DOM, a dodawanie nowych postów jest zablokowane.</p>
-                    <br/>
-                    <p>Ponadto CMS automatycznie losuje zdjęcie z bazy. W pełnej wersji zalogowany użytkownik może dodawać własne pliki z dysku i zapisywać je w chmurze Firebase.</p>
-                </div>}
-                <div className="login-buttons">
-                    {isDemo &&
-                    <button className='btn' onClick={() => history.push('/')}>Wypróbuj</button>}
-                    <button className='btn' onClick={logout}>Wyloguj</button>
-                </div>
-            </div>}
+            {(isLogged || isDemo) && <LoginInfo isDemo={isDemo}/>}
             {(!isLogged && !isDemo) && <LoginForm/>}
         </div>
     );
---
+++ src/js/components/elements/LoginInfo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import firebase from "firebase";
+import {fetchDemo} from "../../redux/actions/allFetchers";
+import {useHistory} from "react-router-dom";
+import {useDispatch} from "react-redux";
+
+const LoginInfo = ({isDemo}) => {
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const logout = () => {
+        firebase.auth().signOut().catch(err => console.error(err.message));
+        dispatch(fetchDemo(false));
+    };
+    return (
+        <div className='login-status'>
+            <h2>Zalogowano jako:</h2>
+            <h3>{isDemo ? 'Wersja demo' : 'Administrator'}</h3>
+            {isDemo &&
+            <div className='login-info' >
+                <p>W wersji demonstracyjnej dostęp do edytora i kreatora postów jest ograniczony. Wpisy modyfikowane są wyłącznie w ramach drzewa DOM, a dodawanie nowych postów jest zablokowane.</p>
+                <br/>
+                <p>Ponadto CMS automatycznie losuje zdjęcie z bazy. W pełnej wersji zalogowany użytkownik może dodawać własne pliki z dysku i zapisywać je w chmurze Firebase.</p>
+            </div>}
+            <div className="login-buttons">
+                {isDemo &&
+                <button className='btn' onClick={() => history.push('/')}>Wypróbuj</button>}
+                <button className='btn' onClick={logout}>Wyloguj</button>
+            </div>
+        </div>
+    );
+}
+
+export default LoginInfo;
