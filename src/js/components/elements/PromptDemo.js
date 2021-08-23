36d1217d21b6da9295042355e6f67a54117d76ba
Morfozaur <e.borzechowski@gmail.com>
2021-08-23 23:37:30 +0200

    Poprawa komunikatu w wersji demo

--- src/js/components/elements/PromptDemo.js
+++ src/js/components/elements/PromptDemo.js
@@ -4,7 +4,7 @@ const PromptDemo = () => {
     return (
         <div className='remove-frame'>
             <h2>Zapisanie zablokowane</h2>
-            <p>Wersja demonstracyjna umożliwia jedynie edycję już opublikowanych wpisów. Po zalogowaniu CMS wysyła do Firebase'a nowy dokument, który natychmiast jest renderowany na stronie z aktualną datą na górze strony.</p>
+            <p>Wersja demonstracyjna umożliwia jedynie edycję już opublikowanych wpisów. Po zalogowaniu CMS wysyła do Firebase'a nowy dokument, który natychmiast jest renderowany z aktualną datą na górze strony.</p>
         </div>
     );
 }
